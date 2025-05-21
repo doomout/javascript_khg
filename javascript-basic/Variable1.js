25e4aa2832f69fa269ac1a2e02a5391baac3eb21
khg <winquake3@hanmail.net>
2025-05-21 23:06:10 +0900

    변수 선언법, 주의점 기록

---
+++ javascript-basic/Variable1.js
@@ -0,0 +1,11 @@
+//변수 예제
+
+// 1. 변수 선언
+let string = "Hello, Variable!";
+console.log(string); // Hello, Variable!
+
+// 변수명에는 특수문자는 _와 $만 사용 가능, 숫자로 시작할 수 없음, 예약어도 불가능
+// 유니코드까지도 변수명으로 사용 가능
+let 한글 = "안녕하세요";
+console.log(한글); // 안녕하세요
+
