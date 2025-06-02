01eaba9503309108ac1bf06c008bc18e134b1367
khg <winquake3@hanmail.net>
2025-06-03 01:31:29 +0900

    switch 문에서 스코프 주의점

--- javascript-Deep/closure.js
+++ javascript-Deep/closure.js
@@ -20,10 +20,41 @@ const func1 = (msg) => {
 // 에러의 이유: 변수 b는 if문 {} 에서 선언되었고,
 // const 는 블록 스코프이기에 if문 밖에서는 변수 b에 접근 할 수 없다.
 // 이처럼 호출된 위치에 따라 접근할 수 있는 값이 달라진다면 동적 스코프를 따른다.
+
 const func2 = () => {
     console.log(b); // ReferenceError: b is not defined
 };
 if (true) {
     const b = 1; // b는 블록 스코프이기에 if문 에서만 사용 가능
     func2();
+}
+
+// 아래 switch 문에선 qwer 모두 name이름 변수를 사용해서 에러가 난다.
+// 이를 해결 하기 위해서는 블록{} 으로 묶어줘야 한다.
+const type = 'w';
+switch (type) {
+    case 'q': {
+        let name = '쵸단'
+        console.log(name);
+        break;
+    }
+    case 'w': {
+        let name = '마젠타'
+        console.log(name);
+        break;
+    }
+    case 'e': {
+        let name = '히나'
+        console.log(name);
+        break;
+    }
+    case 'r': {
+        let name = '시연'
+        console.log(name);
+        break;
+    }
+    default:
+        let name = 'QWER'
+        console.log(name);
+        break;
 }
